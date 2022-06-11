ec87c7ac5a74908b2f280c4a09bdb07d27e65166
Faysal Mridha <fmridha166@.com>
2022-06-11 20:19:02 +0600

    all work done alhamdulilah

--- components/Templete1/TempBanner1.js
+++ components/Templete1/TempBanner1.js
@@ -34,7 +34,7 @@ function TempBanner1({ user }) {
           </div>
         </div>
         {/* image content */}
-        <div className="flex items-center justify-center mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+        <div className="userImage flex items-center justify-center mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
           {/* image */}
           <img
             src={user?.avatar_url}
